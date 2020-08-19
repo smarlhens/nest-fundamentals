5687d440a97e29d394999fcfe61e2ff6259dd300
Samuel MARLHENS <samuel.marlhens@gmail.com>
2020-08-19 09:56:00 +0200

    chore(chap-2): implement pagination w/ query parameters

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -6,13 +6,15 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get('flavors')
-  public findAll() {
-    return 'This action returns all coffees';
+  @Get()
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
+    return `This action returns all coffees. Limit ${limit}, offset: ${offset}`;
   }
 
   @Get(':id')
