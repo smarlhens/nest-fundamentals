6ab4eabfad8fea7fafd9a34526e3ce9baaf8ea15
Samuel MARLHENS <samuel.marlhens@gmail.com>
2021-01-05 21:37:50 +0100

    chore(chap-7): adding example responses

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -19,6 +19,7 @@ import { Request } from '@nestjs/common';
 import { Public } from '../common/decorators/public.decorator';
 import { ParseIntPipe } from '../common/pipes/parse-int.pipe';
 import { Protocol } from '../common/decorators/protocol.decorator';
+import { ApiForbiddenResponse } from '@nestjs/swagger';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -29,6 +30,7 @@ export class CoffeesController {
     console.log('CoffeesController created');
   }
 
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
   @Public()
   @Get()
   async findAll(@Protocol('https') protocol: string, @Query() paginationQuery: PaginationQueryDto): Promise<Coffee[]> {
