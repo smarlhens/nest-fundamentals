62d378b8c0ad98f7d304fae115fd23c51c331355
Samuel MARLHENS <samuel.marlhens@gmail.com>
2021-01-05 21:38:46 +0100

    chore(chap-7): using tags to group resources

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -19,8 +19,9 @@ import { Request } from '@nestjs/common';
 import { Public } from '../common/decorators/public.decorator';
 import { ParseIntPipe } from '../common/pipes/parse-int.pipe';
 import { Protocol } from '../common/decorators/protocol.decorator';
-import { ApiForbiddenResponse } from '@nestjs/swagger';
+import { ApiForbiddenResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
   constructor(
