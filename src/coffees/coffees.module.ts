4daeb920388024c6861e368dbf773ce9638884f5
Samuel MARLHENS <samuel.marlhens@gmail.com>
2021-01-07 19:44:24 +0100

    chore(chap-9): use transactions

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -3,6 +3,7 @@ import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Coffee, CoffeeSchema } from './entities/coffee.entity';
+import { Event, EventSchema } from '../events/entities/event.entity';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { Coffee, CoffeeSchema } from './entities/coffee.entity';
         name: Coffee.name,
         schema: CoffeeSchema,
       },
+      { name: Event.name, schema: EventSchema },
     ]),
   ],
   controllers: [CoffeesController],
