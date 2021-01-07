00d53a112c86206cfc6c9d6864cbbb55a720049e
Samuel MARLHENS <samuel.marlhens@gmail.com>
2021-01-07 19:19:46 +0100

    chore(chap-9): introducing the mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-course'),
+    CoffeesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
